b7677c49daf079069f8bcba701fbcdb99e3850f7
guerriers <faiycoc1508@gmail.com>
2023-08-31 13:27:11 +0700

    setup-git setup-frontend/backend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './rns_logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <p>
+          RNS Brandname
+        </p> */}
+        <a
+          className="App-link"
+          // href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coming soon...
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
