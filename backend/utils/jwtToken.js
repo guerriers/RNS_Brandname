bbea2b0ee584786867aa1d39a8ebdb80f3b46913
guerriers <faiycoc1508@gmail.com>
2023-10-12 14:46:26 +0700

    "add userVerify backend"

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,19 +1,29 @@
 const sendToken = (user, statusCode, res) => {
-    //create jwt token
-    const token = user.getJwtToken();
-    // option for cookie
-    const options = {
-      expires: new Date(
-        Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-    };
-  
-    res.status(statusCode).cookie("token", token, options).json({
-      success: true,
-      token,
-      user,
-    });
-  };
-  
-  module.exports = sendToken;
+  const jwt = require("jsonwebtoken");
+  //create jwt token
+  // const token = user.getJwtToken();
+  const token = jwt.sign(
+    { id: user.id, status: user.status },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h", // Token expires in 1 hours
+    }
+  );
+
+  res.cookie("token", token, {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  });
+
+  // Hide from the response
+  user.email = undefined;
+  user.phone = undefined;
+  // user.f_name = undefined;
+  user.l_name = undefined;
+  user.password = undefined;
+  res.status(statusCode).json({ success: true, token, user });
+};
+
+module.exports = sendToken;
