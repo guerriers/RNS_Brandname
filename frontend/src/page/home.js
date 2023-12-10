d8b1aa114bc6437e5882e392230eb3630c4b335e
Rachen Chaisri <fordwoof9@outlook.com>
2023-12-11 01:51:58 +0700

    Update Home Page

--- frontend/src/page/home.js
+++ frontend/src/page/home.js
@@ -36,19 +36,19 @@ function HomePage() {
 
   const [info, setinfo] = useState([
     {
-      name: 'การบริการที่ยอดเยี่ยม',
+      name: 'เว็บไซต์ตัวกลาง',
       image: "../assets/cart_icon.png",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus nulla dui, in dapibus mauris tristique id."
+      desc: "เว็บไซต์ของเราเป็นแค่เว็บไซต์ตัวกลางที่ช่วยให้พ่อค้า-แม่ค้าทุกคนสามารถมาลงขายสินค้าได้ โดยเราจะไม่มีส่วนได้ส่วนเสีย และจะไม่รับผิดชอบใดๆ"
     },
     {
       name: 'มีบริการเช่าสินค้า',
       image: "../assets/cart_icon.png",
-      desc: "สามารถเช่าสามารถเช่าสามารถเช่าสามารถเช่าสามารถเช่าสามารถเช่าสามารถเช่าสามารถเช่าสามารถเช่าสามารถเช่า"
+      desc: "นอกจากซื้อและขายสินค้ามือสองแล้วยังสามารถเช่าสินค้าแบรนด์เนมได้ด้วย สำหรับคนที่ต้องการเช่ามาใช้ชั่วคราว"
     },
     {
-      name: 'ซื้อ-ขาย สินค้าได้ง่าย',
+      name: 'สินค้าหลากหลายแบรนด์',
       image: "../assets/cart_icon.png",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur rhoncus nulla dui, in dapibus mauris tristique id."
+      desc: "เรามีสินค้าหลากหลายแบรนด์ ไม่ว่าจะเป็น แบรนด์เอ แบรนด์บี แบรนด์ซี และแบรนด์ดี"
     },
   ]);
 
@@ -76,18 +76,21 @@ function HomePage() {
       
 
       <main className='main'>
-      <section className="info-section">
-        <div className="info-lists">
-          {info.map((list) => (
-            <div key={list.name} className="info-list">
-              <img src={list.image} alt={list.name} />
-              <h4>{list.name}</h4>
-              <p>{list.desc}</p>
-            </div>
-          ))}
-        </div>
-      </section>
-        <h1 className="decorated-h1">Featured Categories</h1>
+        <section className="info-section">
+          <div className="info-lists">
+            {info.map((list) => (
+              <div key={list.name} className="info-list">
+                <img src={list.image} alt={list.name} />
+                <h4>{list.name}</h4>
+                <p>{list.desc}</p>
+              </div>
+            ))}
+          </div>
+        </section>
+
+        <hr className='h' />
+      
+        <h1 className="decorated-h1">Best Seller</h1>
         <section className="categories">
           <div className="category" onClick={() => handleCategoryClick('Category 1')}>
             <img src="../assets/p2.jpeg" alt="Category 1" />
@@ -95,29 +98,92 @@ function HomePage() {
           </div>
 
           <div className="category" onClick={() => handleCategoryClick('Category 2')}>
-            <img src="../assets/p2.jpeg" alt="Category 2" />
+            <img src="../assets/BestSeller2.png" alt="Category 2" />
             <p>Category 2</p>
           </div>
 
           <div className="category" onClick={() => handleCategoryClick('Category 3')}>
-            <img src="../assets/p2.jpeg" alt="Category 3" />
+            <img src="../assets/BestSeller3.png" alt="Category 3" />
             <p>Category 3</p>
           </div>
 
           <div className="category" onClick={() => handleCategoryClick('Category 4')}>
-            <img src="../assets/p2.jpeg" alt="Category 4" />
+            <img src="../assets/BestSeller4.png" alt="Category 4" />
             <p>Category 4</p>
+          </div>
+
+          <div className="category" onClick={() => handleCategoryClick('Category 5')}>
+            <img src="../assets/p2.jpeg" alt="Category 5" />
+            <p>Category 5</p>
+          </div>
+        </section>
+
+        <hr className='h' />
+
+        <h1 className="decorated-h1">Explore by Brand</h1>
+        <section className="brands">
+          <div className="brand" onClick={() => handleCategoryClick('brand 1')}>
+            <img src="../assets/p2.jpeg" alt="brand 1" />
+            <p>brand 1</p>
+          </div>
+
+          <div className="brand" onClick={() => handleCategoryClick('gucci')}>
+            <img src="../assets/brand_gucci.png" alt="gucci" />
+            <p>GUCCI</p>
+          </div>
+
+          <div className="brand" onClick={() => handleCategoryClick('dior')}>
+            <img src="../assets/brand_dior.png" alt="dior" />
+            <p>DIOR</p>
+          </div>
+
+          <div className="brand" onClick={() => handleCategoryClick('louisvuitton')}>
+            <img src="../assets/brand_lv.png" alt="louisvuitton" />
+            <p>LOUIS VUITTON</p>
+          </div>
+
+          <div className="brand" onClick={() => handleCategoryClick('brand 5')}>
+            <img src="../assets/p2.jpeg" alt="brand 4" />
+            <p>brand 5</p>
+          </div>
+        </section>
+
+        <hr className='h' />
+      
+        <h1 className="decorated-h1">Explore by Category</h1>
+        <section className="categories">
+          <div className="category" onClick={() => handleCategoryClick('Category 1')}>
+            <img src="../assets/p2.jpeg" alt="Category 1" />
+            <p>Category 1</p>
+          </div>
+
+          <div className="category" onClick={() => handleCategoryClick('Category 2')}>
+            <img src="../assets/category_bag.png" alt="Category 2" />
+            <p>Bag</p>
+          </div>
+
+          <div className="category" onClick={() => handleCategoryClick('Category 3')}>
+            <img src="../assets/category_clothes.png" alt="Category 3" />
+            <p>Clothes</p>
+          </div>
+
+          <div className="category" onClick={() => handleCategoryClick('Category 4')}>
+            <img src="../assets/category_accessories.png" alt="Category 4" />
+            <p>Accessories</p>
           </div>
 
           <div className="category" onClick={() => handleCategoryClick('Category 5')}>
             <img src="../assets/p2.jpeg" alt="Category 4" />
             <p>Category 5</p>
           </div>
-
         </section>
+
       </main>
 
-      <footer>
+      <footer className='homefooter'>
+        <div className='footer-banner'>
+          <img src="../assets/footerbanner.png" alt="FooterBanner" />
+        </div>
         <p>&copy; {new Date().getFullYear()} RNS</p>
       </footer>
     </div>
