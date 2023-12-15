5fca55c7d68fdde8eabe736c4c731e59cd6a0758
guerriers <faiycoc1508@gmail.com>
2023-12-16 00:53:35 +0700

    update myprofile

--- frontend/src/page/myProfile.js
+++ frontend/src/page/myProfile.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Form, Col, Container, Button } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import "../css/myProfile.css";
-import axios from 'axios';
+import axios from "axios";
 
 const MyProfile = () => {
   const [profilePreview, setProfilePreview] = useState([]);
@@ -10,6 +10,7 @@ const MyProfile = () => {
   const [oldProfile, setOldProfile] = useState([]);
   const [loading, setLoading] = useState(true);
   const [validated, setValidated] = useState(false);
+  const [editMode, setEditMode] = useState(false);
 
   // Retrieve the user object from Redux store
   const user = useSelector((state) => state.auth.user);
@@ -36,11 +37,14 @@ const MyProfile = () => {
 
         // Make an API call to fetch the initial values
         const token = localStorage.getItem("token");
-        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/users/${user.id}`, {
-          headers: {
-            'Authorization': `Bearer ${token}`,
-          },
-        });
+        const response = await axios.get(
+          `${process.env.REACT_APP_BASE_URL}/api/users/${user.id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
 
         // Use response.data instead of await response.json()
         setFormValues(response.data);
@@ -53,7 +57,6 @@ const MyProfile = () => {
 
     fetchInitialValues();
   }, [user]);
-  
 
   if (loading) {
     return <p>Loading...</p>; // You can replace this with a loading spinner or any other loading indicator
@@ -82,26 +85,26 @@ const MyProfile = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     const form = e.currentTarget;
-  
+
     if (form.checkValidity() === false) {
       setValidated(true);
       return;
     }
-  
+
     try {
       const token = localStorage.getItem("token");
-  
+
       const formData = new FormData();
       formData.append("f_name", formValues.f_name);
       formData.append("l_name", formValues.l_name);
       formData.append("email", formValues.email);
       formData.append("phone", formValues.phone);
-  
+
       // Append files if available
       profile.forEach((file) => {
         formData.append("img", file);
       });
-  
+
       const response = await axios.put(
         `${process.env.REACT_APP_BASE_URL}/api/users/${user.id}`,
         formData,
@@ -112,7 +115,7 @@ const MyProfile = () => {
           },
         }
       );
-  
+
       if (response.status === 200) {
         alert("Edit successfully");
         // Perform any additional actions after a successful edit
@@ -124,124 +127,150 @@ const MyProfile = () => {
       alert("An error occurred while editing. Please try again.");
     }
   };
-  
+
+  const handleEditClick = () => {
+    setEditMode(true);
+  };
+
+  const handleCancelClick = () => {
+    setEditMode(false);
+    setFormValues({
+      profile_img: [],
+      f_name: oldProfile.f_name,
+      l_name: oldProfile.l_name,
+      email: oldProfile.email,
+      phone: oldProfile.phone,
+    });
+  };
 
   return (
     <Container>
       <div>
         <h1 className="title">My Profile</h1>
 
+        <Form>
+          <Form.Group as={Col} controlId="formFileMultiple">
+            <Form.Label>
+              <div className="myProfile-img-grid">
+                <div className="profileImgBox">
+                  {oldProfile.map((img, index) => (
+                    <img
+                      key={`${img.url}-${index}`}
+                      src={img.url}
+                      alt={img.url}
+                      className="mt-3 mr-2"
+                      width="55"
+                      height="52"
+                    />
+                  ))}
+                  <img
+                    className="imagePreview"
+                    src={"../assets/userProfile.png"}
+                  />
 
-            <Form>
-              <Form.Group as={Col} controlId="formFileMultiple">
-                <Form.Label>
-                  <div className="myProfile-img-grid">
-                    <div className="profileImgBox">
-                      {oldProfile.map((img, index) => (
-                        <img
-                          key={`${img.url}-${index}`}
-                          src={img.url}
-                          alt={img.url}
-                          className="mt-3 mr-2"
-                          width="55"
-                          height="52"
-                        />
-                      ))}
-                      <img className="imagePreview" src={"../assets/userProfile.png"} />
-
-                      {profilePreview.map((img) => (
-                        <img
-                          src={img}
-                          key={img}
-                          alt="Images Preview"
-                          className="mt-3 mr-2"
-                          width="55"
-                          height="52"
-                        />
-                      ))}
-                      <div className="addProfileImgActions">
-                        <span className="plus-sign">+</span>
-                      </div>
-                    </div>
+                  {profilePreview.map((img) => (
+                    <img
+                      src={img}
+                      key={img}
+                      alt="Images Preview"
+                      className="mt-3 mr-2"
+                      width="55"
+                      height="52"
+                    />
+                  ))}
+                  <div className="addProfileImgActions">
+                    <span className="plus-sign">+</span>
                   </div>
-                </Form.Label>
-                <Form.Control
-                  type="file"
-                  name="img"
-                  accept="image/*"
-                  onChange={handleChange}
-                  style={{ display: "none" }}
-                  multiple
-                />
-              </Form.Group>
-            </Form>
+                </div>
+              </div>
+            </Form.Label>
+            <Form.Control
+              type="file"
+              name="img"
+              accept="image/*"
+              onChange={handleChange}
+              style={{ display: "none" }}
+              multiple
+            />
+          </Form.Group>
+        </Form>
 
-            <Col>
-              <div className="editProfileForm">
-                <Form.Group controlId="formGridFirstName">
-                  <Form.Label>First Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="First Name"
-                    name="f_name"
-                    value={formValues.f_name}
-                    onChange={(e) =>
-                      setFormValues({ ...formValues, f_name: e.target.value })
-                    }
-                  />
-                </Form.Group>
+        <Col>
+          <div className="editProfileForm">
+            <Form.Group controlId="formGridFirstName">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="First Name"
+                name="f_name"
+                value={formValues.f_name}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, f_name: e.target.value })
+                }
+                disabled={!editMode}
+              />
+            </Form.Group>
 
-                <Form.Group controlId="formGridLastName">
-                  <Form.Label>Last Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Last Name"
-                    name="l_name"
-                    value={formValues.l_name}
-                    onChange={(e) =>
-                      setFormValues({ ...formValues, l_name: e.target.value })
-                    }
-                  />
-                </Form.Group>
+            <Form.Group controlId="formGridLastName">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Last Name"
+                name="l_name"
+                value={formValues.l_name}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, l_name: e.target.value })
+                }
+                disabled={!editMode}
+              />
+            </Form.Group>
 
-                <Form.Group controlId="formGridEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Email"
-                    name="email"
-                    value={formValues.email}
-                    onChange={(e) =>
-                      setFormValues({ ...formValues, email: e.target.value })
-                    }
-                  />
-                </Form.Group>
+            <Form.Group controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Email"
+                name="email"
+                value={formValues.email}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, email: e.target.value })
+                }
+                disabled={!editMode}
+              />
+            </Form.Group>
 
-                <Form.Group controlId="formGridPhone">
-                  <Form.Label>Phone</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Phone"
-                    name="phone"
-                    value={formValues.phone}
-                    onChange={(e) =>
-                      setFormValues({ ...formValues, phone: e.target.value })
-                    }
-                  />
-                </Form.Group>
+            <Form.Group controlId="formGridPhone">
+              <Form.Label>Phone</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Phone"
+                name="phone"
+                value={formValues.phone}
+                onChange={(e) =>
+                  setFormValues({ ...formValues, phone: e.target.value })
+                }
+                disabled={!editMode}
+              />
+            </Form.Group>
 
-                <div className="editProfileBtn">
+            <div className="editProfileBtn">
+              {editMode ? (
+                <>
                   <Button variant="primary" type="submit" onClick={onSubmit}>
                     Save
                   </Button>
-                </div>
-              </div>
-            </Col>
-
-            
-
-
-
+                  <Button variant="secondary" onClick={handleCancelClick}>
+                    Cancel
+                  </Button>
+                </>
+              ) : (
+                <Button variant="primary" onClick={handleEditClick}>
+                  Edit
+                </Button>
+              )}
+            </div>
+          </div>
+        </Col>
       </div>
     </Container>
   );
